47fe6e6246bfa8b5a38eb21e4e4dc242ade5bc27
datasauces <datasauces@proton.me>
2023-02-12 15:00:09 +0000

    Improved error handling.

--- Plugins/GlennvWsNLPBlueprintLibrary/HostProject/Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibrary.init.gen.cpp
+++ Plugins/GlennvWsNLPBlueprintLibrary/HostProject/Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibrary.init.gen.cpp
@@ -1,3 +1,5 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 /*===========================================================================
-	Generated code exported
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+========================================================================
