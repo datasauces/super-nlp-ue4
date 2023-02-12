8f04ba0bf82a47cc9b233885c12c65ce92874249
datasauces <datasauces@proton.me>
2023-02-12 19:54:13 +0000

    Add TravisCI support

--- Plugins/GlennvWsNLPBlueprintLibrary/HostProject/Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibrary.init.gen.cpp
+++ Plugins/GlennvWsNLPBlueprintLibrary/HostProject/Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibrary.init.gen.cpp
@@ -2,4 +2,11 @@
 /*===========================================================================
 	Generated code exported from UnrealHeaderTool.
 	DO NOT modify this manually! Edit the corresponding .h files instead!
-========================================================================
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNI
