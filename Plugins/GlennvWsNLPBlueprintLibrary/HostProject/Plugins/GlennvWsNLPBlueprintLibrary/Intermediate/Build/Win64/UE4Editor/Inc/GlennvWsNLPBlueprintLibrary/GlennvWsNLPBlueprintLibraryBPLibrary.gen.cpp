dc947053df7bf46a583df23c027379700a0390cb
datasauces <datasauces@proton.me>
2023-02-13 19:24:59 +0000

    Pass parameter by const ref instead of const value.

--- Plugins/GlennvWsNLPBlueprintLibrary/HostProject/Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibraryBPLibrary.gen.cpp
+++ Plugins/GlennvWsNLPBlueprintLibrary/HostProject/Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibraryBPLibrary.gen.cpp
@@ -1,3 +1,5 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 /*===========================================================================
-	Genera
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===============================================================
