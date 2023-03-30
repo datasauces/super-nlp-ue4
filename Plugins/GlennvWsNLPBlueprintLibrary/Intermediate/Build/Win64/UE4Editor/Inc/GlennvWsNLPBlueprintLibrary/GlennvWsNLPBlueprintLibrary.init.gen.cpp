61fe0813afd35213e2422b0d8b0196ba0bc9730a
datasauces <datasauces@proton.me>
2023-03-30 12:35:01 +0000

    Rename .java to .kt

--- Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibrary.init.gen.cpp
+++ Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibrary.init.gen.cpp
@@ -23,3 +23,11 @@ void EmptyLinkFunctionForGeneratedCodeGlennvWsNLPBlueprintLibrary_init() {}
 				0x7C29324F,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
+			};
+			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
+		}
+		return ReturnPackage;
+	}
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (po
