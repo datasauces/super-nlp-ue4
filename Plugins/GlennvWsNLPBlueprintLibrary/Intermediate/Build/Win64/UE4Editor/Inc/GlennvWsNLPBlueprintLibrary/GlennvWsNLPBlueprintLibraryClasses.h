53d68bbbb528c9ed674e9346ace82a6acfe395d7
datasauces <datasauces@proton.me>
2023-03-31 06:55:47 +0000

    ignore some stuff

--- Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibraryClasses.h
+++ Plugins/GlennvWsNLPBlueprintLibrary/Intermediate/Build/Win64/UE4Editor/Inc/GlennvWsNLPBlueprintLibrary/GlennvWsNLPBlueprintLibraryClasses.h
@@ -1,3 +1,7 @@
 // Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
 /*===========================================================================
-	Generated code exported from Unr
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#pragma once
