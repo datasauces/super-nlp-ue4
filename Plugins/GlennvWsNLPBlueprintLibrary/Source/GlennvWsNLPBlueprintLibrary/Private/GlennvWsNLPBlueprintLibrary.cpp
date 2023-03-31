e6cc527d2133a91c1e1f64f371eb9b2db535ac6b
datasauces <datasauces@proton.me>
2023-03-31 17:45:13 +0000

    mpi: Fix NULL ptr dereference in mpi_powm() [ver #3]

--- Plugins/GlennvWsNLPBlueprintLibrary/Source/GlennvWsNLPBlueprintLibrary/Private/GlennvWsNLPBlueprintLibrary.cpp
+++ Plugins/GlennvWsNLPBlueprintLibrary/Source/GlennvWsNLPBlueprintLibrary/Private/GlennvWsNLPBlueprintLibrary.cpp
@@ -6,4 +6,10 @@
 
 void FGlennvWsNLPBlueprintLibraryModule::StartupModule()
 {
-	// This code will execute aft
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+	
+}
+
+void FGlennvWsNLPBlueprintLibraryModule::ShutdownModule()
+{
+	
