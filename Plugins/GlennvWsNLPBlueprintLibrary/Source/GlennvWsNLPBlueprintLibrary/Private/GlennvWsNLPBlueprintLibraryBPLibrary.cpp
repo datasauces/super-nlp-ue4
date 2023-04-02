cdb89bc54ab3909e7a39b7c55cd848686e46e6a4
datasauces <datasauces@proton.me>
2023-04-02 11:22:08 +0000

    move refactor

--- Plugins/GlennvWsNLPBlueprintLibrary/Source/GlennvWsNLPBlueprintLibrary/Private/GlennvWsNLPBlueprintLibraryBPLibrary.cpp
+++ Plugins/GlennvWsNLPBlueprintLibrary/Source/GlennvWsNLPBlueprintLibrary/Private/GlennvWsNLPBlueprintLibraryBPLibrary.cpp
@@ -1,1 +1,6 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reser
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "GlennvWsNLPBlueprintLibraryBPLibrary.h"
+#include "GlennvWsNLPBlueprintLibrary.h"
+#include "Runtime/Core/Public/Misc/Paths.h"
+#include "Runtim
